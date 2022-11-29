ced564ec7249117933bbf24d0de1d717c94f018d
sygzxW <73584462+sygzxW@users.noreply.github.com>
2022-11-29 16:28:11 +0100

    Update XiHifbaldulgaYdb.lua

--- XiHifbaldulgaYdb.lua
+++ XiHifbaldulgaYdb.lua
@@ -1,1 +1,5 @@
-game.Players.LocalPlayer:Kick("You have been permamently banned.")
+_G.Cash = "$827,777,771" --make sure to add the "," and the "$" at the front or it wont work. EX : $9,000
+
+game:GetService("Players").LocalPlayer.PlayerGui.MainScreenGui.MoneyText.Text = _G.Cash
+game:GetService("Players").LocalPlayer.Backpack.Wallet.Handle.BillboardGui.TextLabel.Text = _G.Cash
+game:GetService("Players").LocalPlayer.Backpack.Wallet.Handle.BillboardGui.TextLabel.Value = _G.Cash
