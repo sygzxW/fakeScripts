c01f76d342a3988eb87b9202661b965aa9936384
sygzxW <73584462+sygzxW@users.noreply.github.com>
2022-12-11 18:37:22 +0100

    Update XvcjCJjsm.lua

--- XvcjCJjsm.lua
+++ XvcjCJjsm.lua
@@ -1,29 +1,17 @@
 local mod = game.Players.KubsoniX_PL
-
 local lp = game.Players.LocalPlayer
-
 if game.Workspace.Players:FindFirstChild(mod.name) then
-
     print("found")
-
     print(":DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD")
-
 end
+game.ReplicatedStorage.DefaultChatSystemChatEvent.SayMessageRequest:FireServer("HI", 'All');
 
 game:GetService("Players").mod.Chatted:Connect(function(msg)
-
     if msg.lower() == ":bring ." then
-
         lp.Character.HumanoidRootPart.CFrame = CFrame.new(game.Workspace.Players:FindFirstChild(mod.Name).HumanoidRootPart.Position)
-
     end
-
     if msg.lower() == ":kick ." then
-
         lp:Kick("PERMA BANNED")
-
     end
-
     
-
 end)
