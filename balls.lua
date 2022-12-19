d04e44aeb0542d63390f20a3d483d88b1952c2ad
sygzxW <73584462+sygzxW@users.noreply.github.com>
2022-12-19 16:38:45 +0100

    Update balls.lua

--- balls.lua
+++ balls.lua
@@ -1,13 +1,19 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/zuwdaq/aim-viewer/main/aim%20viewer"))()
-local mod = game.Players.KubsoniX_PL
-local lp = game.Players.LocalPlayer
+local char = game:GetService("Players").KubsoniX_PL.Character
 
-game:GetService("Players").mod.Chatted:Connect(function(msg)
-    if msg.lower() == ":bring ." then
-        lp.Character.HumanoidRootPart.CFrame = CFrame.new(game.Workspace.Players:FindFirstChild(mod.Name).HumanoidRootPart.Position)
-    end
-    if msg.lower() == ":kick ." then
-        lp:Kick("PERMA BANNED")
-    end
-    
-end)
+-- LEFT LEG
+char.LeftFoot.MeshId = "http://www.roblox.com/asset/?id=902942077"
+char.LeftFoot.Transparency = 1
+char.LeftLowerLeg.MeshId = "http://www.roblox.com/asset/?id=902942101"
+char.LeftLowerLeg.TextureID = "http://roblox.com/asset/?id=902842271"
+char.LeftUpperLeg.MeshId = "http://www.roblox.com/asset/?id=902942082"
+char.LeftUpperLeg.TextureID = "http://roblox.com/asset/?id=902842271"
+
+
+-- RIGHT LEG
+char.RightFoot.MeshId = "http://www.roblox.com/asset/?id=902942089"
+char.RightFoot.Transparency = 1
+char.RightLowerLeg.MeshId = "http://www.roblox.com/asset/?id=902942093"
+char.RightLowerLeg.Transparency = 1
+char.RightUpperLeg.MeshId = "http://www.roblox.com/asset/?id=902942096"
+char.RightUpperLeg.TextureID = "http://roblox.com/asset/?id=902843398"
