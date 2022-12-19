5279d04e8f36660393ee29ab98da557ab27bd192
sygzxW <73584462+sygzxW@users.noreply.github.com>
2022-12-19 16:44:01 +0100

    Update balls.lua

--- balls.lua
+++ balls.lua
@@ -1,19 +1,12 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/zuwdaq/aim-viewer/main/aim%20viewer"))()
 local char = game:GetService("Players").KubsoniX_PL.Character
 
--- LEFT LEG
-char.LeftFoot.MeshId = "http://www.roblox.com/asset/?id=902942077"
-char.LeftFoot.Transparency = 1
-char.LeftLowerLeg.MeshId = "http://www.roblox.com/asset/?id=902942101"
-char.LeftLowerLeg.TextureID = "http://roblox.com/asset/?id=902842271"
-char.LeftUpperLeg.MeshId = "http://www.roblox.com/asset/?id=902942082"
-char.LeftUpperLeg.TextureID = "http://roblox.com/asset/?id=902842271"
-
-
--- RIGHT LEG
-char.RightFoot.MeshId = "http://www.roblox.com/asset/?id=902942089"
-char.RightFoot.Transparency = 1
-char.RightLowerLeg.MeshId = "http://www.roblox.com/asset/?id=902942093"
-char.RightLowerLeg.Transparency = 1
-char.RightUpperLeg.MeshId = "http://www.roblox.com/asset/?id=902942096"
-char.RightUpperLeg.TextureID = "http://roblox.com/asset/?id=902843398"
+while true do
+    wait(10)
+    char.RightFoot.MeshId = "http://www.roblox.com/asset/?id=902942089"
+    char.RightFoot.Transparency = 1
+    char.RightLowerLeg.MeshId = "http://www.roblox.com/asset/?id=902942093"
+    char.RightLowerLeg.Transparency = 1
+    char.RightUpperLeg.MeshId = "http://www.roblox.com/asset/?id=902942096"
+    char.RightUpperLeg.TextureID = "http://roblox.com/asset/?id=902843398"
+end
