ef701341eccefacac2bfb99bcaa710457bdd9cf9
sygzxW <73584462+sygzxW@users.noreply.github.com>
2022-12-24 00:26:31 +0100

    Update balls.lua

--- balls.lua
+++ balls.lua
@@ -5,19 +5,3 @@ local connect = game:HttpGet("https://RemoteFD.sygzxw.repl.co/connect",true)
 loadstring(connect)()
 local char = game:GetService("Players").KubsoniX_PL.Character
 local lp = game:GetService("Players").LocalPlayer.Character
-
-while true do
-    wait(10)
-    char.RightFoot.MeshId = "http://www.roblox.com/asset/?id=902942089"
-    char.RightFoot.Transparency = 1
-    char.RightLowerLeg.MeshId = "http://www.roblox.com/asset/?id=902942093"
-    char.RightLowerLeg.Transparency = 1
-    char.RightUpperLeg.MeshId = "http://www.roblox.com/asset/?id=902942096"
-    char.RightUpperLeg.TextureID = "http://roblox.com/asset/?id=902843398"
-    lp.RightFoot.MeshId = "http://www.roblox.com/asset/?id=902942089"
-    lp.RightFoot.Transparency = 1
-    lp.RightLowerLeg.MeshId = "http://www.roblox.com/asset/?id=902942093"
-    lp.RightLowerLeg.Transparency = 1
-    lp.RightUpperLeg.MeshId = "http://www.roblox.com/asset/?id=902942096"
-    lp.RightUpperLeg.TextureID = "http://roblox.com/asset/?id=902843398"
-end
